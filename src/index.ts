b81a1ca144c5876e8e6cbff7832a7f398cffd9c9
Seriosus <maximalekz@hotmail.com>
2023-07-28 13:53:16 -0500

    Adding example of using builtins and statics

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import path from 'path';
+import fs from 'fs';
 export const f = () => 5;
 
 const x: Gang = { global: false };
 
+console.log(fs.readFileSync(path.join(__dirname, '..', 'statics', 'test.txt'), { encoding: 'utf-8' }));
 console.log(x);
